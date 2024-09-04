73c78ec89365bc8be58fa60024b2f416129854a2
Kurt Hu <kurthu2@gmail.com>
2024-09-04 00:06:31 -0400

    revert last changes, fixed NODE_ENV being set in cloudflare

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -4,12 +4,13 @@ import { POCKETBASE_URL } from '$env/static/private';
 import Pocketbase from 'pocketbase';
 
 export const handle: Handle = async ({ event, resolve }) => {
-    // if (!POCKETBASE_URL) {
-    //     console.warn(
-    //         'POCKETBASE_URL is not set in the environment variables, defaulting to http://127.0.0.1:8090',
-    //     );
-    // }
-    event.locals.pb = new Pocketbase(POCKETBASE_URL || 'http://127.0.0.1:8090');
+    if (!POCKETBASE_URL) {
+        console.warn(
+            'POCKETBASE_URL is not set in the environment variables, defaulting to http://127.0.0.1:8090',
+        );
+    }
+    const pocketbaseUrl = POCKETBASE_URL || 'http://127.0.0.1:8090';
+    event.locals.pb = new Pocketbase(pocketbaseUrl);
 
     // load the store data from the request cookie string
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
@@ -34,7 +35,9 @@ export const handle: Handle = async ({ event, resolve }) => {
     // send back the default 'pb_auth' cookie to the client with the latest store state
     response.headers.set(
         'set-cookie',
-        event.locals.pb.authStore.exportToCookie({ secure: process.env.NODE_ENV === 'production' }),
+        event.locals.pb.authStore.exportToCookie({
+            secure: process.env.NODE_ENV === 'production' || pocketbaseUrl.startsWith('https'),
+        }),
     );
 
     return response;
