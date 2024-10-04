672aa9d77a03416c4fea3175dd5b8cc810134825
Kurt Hu <kurthu2@gmail.com>
2024-10-04 00:14:19 -0400

    Setup new tables to prep for srs

--- server/README.md
+++ server/README.md
@@ -6,3 +6,12 @@
 ## Development
 
 Once Go is installed, run `go run main.go serve`, which will start the server and initialize the local database if it does not already exist.
+
+## Spaced Repetition Algorithm
+
+### References
+
+- https://www.reddit.com/r/Anki/comments/1c29775/fsrs_is_one_of_the_most_accurate_spaced
+- https://github.com/open-spaced-repetition/fsrs4anki/wiki/The-Algorithm
+- https://github.com/open-spaced-repetition/go-fsrs
+  - https://github.com/open-spaced-repetition/go-fsrs/blob/main/models.go
--- server/main.go
+++ server/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v5"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
@@ -15,6 +18,17 @@ func main() {
     // serves static files from the provided public dir (if exists)
     app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
         e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+        return nil
+    })
+
+    // docs: https://pocketbase.io/docs/go-routing/#registering-new-routes
+    app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+        e.Router.GET("/api/biblemem/", func(c echo.Context) error {
+            name := c.PathParam("name")
+    
+            return c.JSON(http.StatusOK, map[string]string{"message": "Hello " + name})
+        }, /* optional middlewares */)
+    
         return nil
     })
 
